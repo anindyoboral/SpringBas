3c9238eb2e4eb374f4abdd1a56eddef41cea11de
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2023-02-08 17:05:25 +0100

    spring core

---
+++ Exercise22/src/main/java/be/abis/exercise/Exercise22Application.java
@@ -0,0 +1,30 @@
+package be.abis.exercise;
+
+import be.abis.exercise.service.AbisTrainingService;
+import be.abis.exercise.service.CourseService;
+import be.abis.exercise.service.PersonService;
+import be.abis.exercise.service.TrainingService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Exercise22Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise22Application.class, args);
+    }
+
+
+    @Bean
+    public TrainingService trainingService(PersonService ps, CourseService cs){
+        AbisTrainingService ts = new AbisTrainingService();
+        ts.setCourseService(cs);
+        ts.setPersonService(ps);
+        return ts;
+    }
+
+}
