9788be5832907cab91bd55fad2bd574d5a15f510
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2023-02-10 08:58:22 +0100

    AOP

---
+++ Exercise42/src/main/java/be/abis/exercise/Exercise42Application.java
@@ -0,0 +1,28 @@
+package be.abis.exercise;
+
+import be.abis.exercise.service.AbisTrainingService;
+import be.abis.exercise.service.CourseService;
+import be.abis.exercise.service.TrainingService;
+import be.abis.exercise.service.PersonService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:my.properties")
+public class Exercise42Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise42Application.class, args);
+    }
+
+    @Bean
+    public TrainingService trainingService(PersonService ps, @Qualifier("abis") CourseService cs) {
+        AbisTrainingService ts = new AbisTrainingService();
+        ts.setCourseService(cs);
+        ts.setPersonService(ps);
+        return ts;
+    }
+}
---
+++ Exercise42/src/test/java/be/abis/exercise/TestSomePersonService.java
@@ -0,0 +1,26 @@
+package be.abis.exercise;
+
+import be.abis.exercise.service.PersonService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+@ActiveProfiles("dev")
+public class TestSomePersonService {
+	
+	@Autowired
+	PersonService personService;
+
+	@Test
+	public void person4ShouldBeCalledSandy(){
+		String firstName = personService.findPerson("sschillebeeckx@abis.be","abc123").getFirstName();
+		assertEquals("Sandy",firstName);
+	}
+
+
+}
